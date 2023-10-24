0eaa03b4f2c4bdf62740f136731cb13b84881a73
stclaire1 <anaccustodio8@gmail.com>
2023-10-24 14:33:10 -0300

    data-base creation

---
+++ src/create-database.sql
@@ -0,0 +1,12 @@
+create database cidadaos;
+use cidadaos;
+
+create table info(
+	id int auto_increment primary key,
+	nome varchar(200),
+    email varchar(200),
+    celular varchar(200)    
+);
+
+insert into info (id, nome, email, celular) 
+values (null, 'Ana Clara', 'anaclara.custodio@estudante.iftm.edu.br', '99999-9999');
